3b43df44ad1c941961be738b585198553b2ba7d9
GreatLikeGame <likehainiu@icloud.com>
2024-06-10 20:07:22 +0800

    middleware and a function to handle  error

--- api/index.js
+++ api/index.js
@@ -20,3 +20,13 @@ app.listen(3000, console.log("server is running on port 3000!"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal serverl error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
