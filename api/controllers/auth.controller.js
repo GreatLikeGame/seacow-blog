dda5d1b5ab77f29879fbe3fca4e027c419ae6c23
GreatLikeGame <likehainiu@icloud.com>
2024-06-10 22:52:10 +0800

    sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   // 记录下来
 
@@ -26,6 +26,36 @@ export const signup = async (req, res, next) => {
     //存起来
     await newUser.save();
     res.json("signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "all fields are required"));
+  }
+  try {
+    //找到邮箱
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "user not found"));
+    }
+    //对比密码
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "invalid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //把密码去掉
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
