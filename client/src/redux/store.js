3b25bf57790f6a4c83f02699d264603c63b6fbec
GreatLikeGame <likehainiu@icloud.com>
2024-06-11 11:30:07 +0800

    redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { userReducer } from "./user/userSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const rootreducer = combineReducers({
+  user: userReducer,
 });
+
+const persistCofig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistCofig, rootreducer);
+export const store = configureStore({
+  reducer: persistedReducer,
+  //防止出现error
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
