bbdc04573c05c81e519d7db9e996fe3ed755d27a
GreatLikeGame <likehainiu@icloud.com>
2024-06-11 14:05:39 +0800

    make the dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,17 +9,21 @@ import Projects from "./pages/Projects";
 import Header from "./components/Header";
 import SignUp from "./pages/SignUp";
 import FooterCom from "./components/Footer";
-
+import PrivateRoute from "./components/PrivateRoute";
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
         <Route path="/projects" element={<Projects />} />
       </Routes>
       <FooterCom />
