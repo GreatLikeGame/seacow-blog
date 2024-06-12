89a10152c77e2931e1f52274bade0ed75268752d
GreatLikeGame <likehainiu@icloud.com>
2024-06-12 17:01:54 +0800

    add admin to user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,21 +7,14 @@ export const signup = async (req, res, next) => {
 
   //   console.log(req.body);
   const { username, email, password } = req.body;
-  if (
-    !username ||
-    !email ||
-    !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
-  ) {
+  if (!email || !password || email === "" || password === "") {
     next(errorHandler(400, "all fields are required"));
   }
 
   //加密
   const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({ username: 123, email, password: hashedPassword });
   try {
     //存起来
     await newUser.save();
@@ -47,7 +40,10 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
       return next(errorHandler(400, "invalid password"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     //把密码去掉
     const { password: pass, ...rest } = validUser._doc;
     res
@@ -71,7 +67,7 @@ export const google = async (req, res, next) => {
         Math.random().toString(36).slice(-8) +
         Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
-      const newUser = new User({
+      const user = new User({
         username:
           name.toLowerCase().split(" ").join("") +
           Math.random().toString(9).slice(-4),
@@ -79,12 +75,15 @@ export const google = async (req, res, next) => {
         password: hashedPassword,
         profilePicture: googlePhotoUrl,
       });
-      await newUser.save();
+      await user.save();
 
       res.json("signup successful");
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT_SECRET
+    );
     const { password, ...rest } = user._doc;
     res
       .status(200)
