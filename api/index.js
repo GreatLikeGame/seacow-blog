947d28869e55cfaf734187c664a4b757f27923fb
GreatLikeGame <likehainiu@icloud.com>
2024-06-12 18:11:02 +0800

    add create post api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import PostRoutes from "./routes/post.route.js";
+
 import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
@@ -22,6 +24,7 @@ app.listen(3000, console.log("server is running on port 3000!"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/post", PostRoutes);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
