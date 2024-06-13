a409a59ee4b26d30962904eb5a65be77e5fdf2dd
GreatLikeGame <likehainiu@icloud.com>
2024-06-13 19:11:43 +0800

    add delete

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -62,11 +62,14 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, "you are not allowed to delete this user"));
   }
+  // console.log(res);
   try {
+    console.log(req.params.userId);
     await User.findByIdAndDelete(req.params.userId);
+
     res.status(200).json("user has been deleted");
   } catch (error) {
     next(error);
--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -21,7 +21,7 @@ export default function DashUsers() {
         const data = await res.json();
         if (res.ok) {
           setUsers(data.users);
-          console.log(data);
+          // console.log(data);
 
           if (data.users.length < 9) {
             setShowMore(false);
@@ -53,25 +53,20 @@ export default function DashUsers() {
   };
 
   const handleDeleteUser = async () => {
-    // setShowModal(false);
-    // try {
-    //   const res = await fetch(
-    //     `/api/post/deleteuser/${userIdToDelete}/${currentUser._id}`,
-    //     {
-    //       method: "DELETE",
-    //     }
-    //   );
-    //   const data = await res.json();
-    //   if (!res.ok) {
-    //     console.log(data.message);
-    //   } else {
-    //     setUserPosts((prev) =>
-    //       prev.filter((post) => post._id !== postIdToDelete)
-    //     );
-    //   }
-    // } catch (error) {
-    //   console.log(error.message);
-    // }
+    try {
+      const res = await fetch(`/api/user/delete/${userIdToDelete}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        setUsers((prev) => prev.filter((user) => user._id !== userIdToDelete));
+        setShowModal(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
   };
   return (
     <div
