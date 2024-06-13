afacd2a76ebd3d11923659f2e4544885c336fa0b
GreatLikeGame <likehainiu@icloud.com>
2024-06-13 21:14:31 +0800

    call to action

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from "flowbite-react";
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -66,9 +67,10 @@ export default function PostPage() {
         className="p-3 max-w-2xl mx-auto w-full post-content"
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
-      {/* <div className='max-w-4xl mx-auto w-full'>
+      <div className="max-w-4xl mx-auto w-full">
         <CallToAction />
       </div>
+      {/*
       <CommentSection postId={post._id} />
 
       <div className='flex flex-col justify-center items-center mb-5'>
