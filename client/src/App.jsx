58df68b0a293c2cdac8c7cce83587cdd70a69ce9
GreatLikeGame <likehainiu@icloud.com>
2024-06-16 12:40:10 +0800

    sign in and app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
-import SignUp from "./pages/SignUp";
+
 import FooterCom from "./components/Footer";
 import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
--- client/src/pages/SignUP.jsx
+++ client/src/pages/SignUP.jsx
@@ -1,6 +1,6 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
-import { Link, Navigate, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
 
 export default function SignUp() {
