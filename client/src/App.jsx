377743298b64d736f7ba49677d08fed2113ee236
GreatLikeGame <likehainiu@icloud.com>
2024-06-16 13:16:12 +0800

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-// import SignUp from "./pages/SignUp";
+import SignU from "./pages/SignU";
 
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
@@ -28,6 +28,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         {/* <Route path="/sign-up" element={<SignUp />} /> */}
+        <Route path="/sign-up" element={<SignU />} />
         <Route path="/search" element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
