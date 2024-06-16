33041d093021305d1696d81ae315d865f85f2524
GreatLikeGame <likehainiu@icloud.com>
2024-06-16 12:53:07 +0800

    singup add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-// import SignUp from "./pages/SignUp";
+import SignUp from "./pages/SignUp";
 
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
